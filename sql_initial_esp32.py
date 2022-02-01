1dffae74706f37c5d9dacc4515abb21002ef4d1d
Paul Zech <paul@zechs.de>
2022-02-01 16:30:42 +0100

    Added sql insert for esp32

---
+++ sql_initial_esp32.py
@@ -0,0 +1,18 @@
+# Use this to add the correct table to your mysql database
+
+import mysql.connector
+
+# Enter here your mysql login data
+db = mysql.connector.connect(
+                host ="localhost",
+                user ="root",
+                password="",
+                database = "messwerte",
+                )
+
+cursor = db.cursor()
+
+# This line is for the Eastron SDM230 Device
+cursor.execute("CREATE TABLE esp32 (`ID` INT AUTO_INCREMENT PRIMARY KEY,`Datum` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,`drehzahl` FLOAT(5,1))")
+
+# Other devices will follow...
