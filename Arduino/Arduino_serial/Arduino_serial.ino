f84e35cf57793cd790563be617e17e3dede66e82
Paul Zech <paul@zechs.de>
2022-02-07 15:42:09 +0100

    First serial communication

---
+++ Arduino/Arduino_serial/Arduino_serial.ino
@@ -0,0 +1,11 @@
+void setup(){
+    Serial.begin(9600);
+}
+ 
+void loop(){
+    if (Serial.available()) {
+        byte nr = Serial.read();
+        Serial.print("Folgender char wurde empfangen: ");
+        Serial.println(nr, DEC);
+    }
+}
---
+++ Arduino_serial.py
@@ -0,0 +1,14 @@
+import serial
+import time
+ 
+s = serial.Serial('/dev/ttyUSB0', 9600) # Namen ggf. anpassen
+s.open()
+time.sleep(5) # der Arduino resettet nach einer Seriellen Verbindung, daher muss kurz gewartet werden
+ 
+s.write("test")
+try:
+    while True:
+        response = s.readline()
+        print(response)
+except KeyboardInterrupt:
+    s.close()
